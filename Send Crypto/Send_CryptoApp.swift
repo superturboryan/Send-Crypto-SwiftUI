374e2e6d2934e31a7a3b4dad9353f2f3f71bcd3a
Ryan Forsyth <forsyth.r@gmail.com>
2023-09-08 12:03:22 +0100

    Initial Commit

---
+++ Send Crypto/Send_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Send_CryptoApp.swift
+//  Send Crypto
+//
+//  Created by Ryan Forsyth on 2023-09-08.
+//
+
+import SwiftUI
+
+@main
+struct Send_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
